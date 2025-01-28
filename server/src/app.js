c4f460e64cc9d3ffe5559618d7a375d0753cba08
coder-mizan01 <p.mizan2441@gmail.com>
2025-01-29 00:36:06 +0600

    add origin to app

--- server/src/app.js
+++ server/src/app.js
@@ -31,7 +31,7 @@ app.use(passport.initialize());
 
 
 const corsOptions = {
-    origin : ["http://localhost:3000", "https://wayvi-server.vercel.app"],
+    origin : ["http://localhost:3000", "https://wayvi-app.vercel.app"],
     credentials  : true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
